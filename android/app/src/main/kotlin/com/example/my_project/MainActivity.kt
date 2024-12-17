8b57a8d10fb0bdf9282bd08d83e160c8281df6ee
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-17 09:24:44 +0000

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.almoond.ialert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,3 @@
+// Export pages
+export '/authentication_page/authentication_page_widget.dart'
+    show AuthenticationPageWidget;
